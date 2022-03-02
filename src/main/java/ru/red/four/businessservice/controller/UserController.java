c819c8aa65bd1b4f3c0dea6a066a2c023bfbeb46
Daniil Shreyder <daniil.schroder@gmail.com>
2022-03-03 00:56:49 +0300

    Minor codestyle

--- src/main/java/ru/red/four/businessservice/controller/UserController.java
+++ src/main/java/ru/red/four/businessservice/controller/UserController.java
@@ -2,11 +2,9 @@ package ru.red.four.businessservice.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
