11b17e0a53750a34a0092b7ad916528e2f3a5fef
Daniil Shreyder <daniil.schroder@gmail.com>
2022-03-03 04:33:29 +0300

    Add JsonSerializer

--- src/main/java/ru/red/four/businessservice/dto/UserDetachedDTO.java
+++ src/main/java/ru/red/four/businessservice/dto/UserDetachedDTO.java
@@ -1,6 +1,8 @@
 package ru.red.four.businessservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,5 +12,6 @@ public class UserDetachedDTO {
     private String username;
     private String description;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate birthDate;
 }
